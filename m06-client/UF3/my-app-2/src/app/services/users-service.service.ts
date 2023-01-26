618df5e6c32e2e10f53ccdbf9514473505a715b7
Victor <45897867+TheVic489@users.noreply.github.com>
2023-01-26 21:24:23 +0000

    working in user-service

--- m06-client/UF3/my-app-2/src/app/services/users-service.service.ts
+++ m06-client/UF3/my-app-2/src/app/services/users-service.service.ts
@@ -5,11 +5,11 @@ import { Injectable } from '@angular/core';
 })
 export class UsersServiceService {
   constructor() {}
-
   myUsersArray: object[] = []
+
   // Métodos que generen/devuelvan datos
   getUsers(): object[]|void {
-    let users:   string[] = [];
+    let users:    string[] = [];
     let auxiliar: string[] = ['manzanas', 'fresas', 'sandias', 'peras'];
 
     for (let i: number = 0; i < 100; i++) {
@@ -20,12 +20,27 @@ export class UsersServiceService {
 
  //  return users;
   }
-  // VAlidate and return role of user
+  // Validate and return role of user
   validateUser(username: string, password: string): string {
-    
+    users.forEach(User => {
+      
+      if (username == dbUsername && password == dbPassword) {
+
+      }
+
+    });
     let role = '';
 
     return  role
+  }
+  
+  validateUsers(users, userToValidate) {
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].username === userToValidate.username && users[i].password === userToValidate.password) {
+        return true;
+      }
+    }
+    return false;
   }
 
   registerUser(user2reg: object): any {
