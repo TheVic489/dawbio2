d44c7d1b9a625f39abe7c68ba29187d0dcb19aad
Victor <45897867+TheVic489@users.noreply.github.com>
2023-02-02 17:18:26 +0100

    Delete m06-client/UF3/my-app-2 directory

--- m06-client/UF3/my-app-2/src/app/directives/validate-rep-pass.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ValidateRepPassDirective } from './validate-rep-pass.directive';
-
-describe('ValidateRepPassDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ValidateRepPassDirective();
-    expect(directive).toBeTruthy();
-  });
-});
